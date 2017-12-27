497f3ac942c498cf11de1a9b40adeb58853d4c35
Krelex <33057745+Krelex@users.noreply.github.com>
2017-12-27 10:16:33 +0100

    Add files via upload

---
+++ project-pixel-art-maker-starter-master/designs.js
@@ -0,0 +1,40 @@
+// Select color input
+
+// Select size input
+
+
+
+
+// When size is submitted by the user, call makeGrid
+function makeGrid() {
+	var pixR= $('#input_height').val();
+	var pixC= $('#input_width').val();
+	var pix= $('#pixel_canvas');
+	pix.children().remove();
+	
+	for (var row=0 ; row < pixR ; row++){
+		pix.append("<tr></tr>");
+		for (var col=0 ; col < pixC ; col++) {
+			pix.children().last().append("<td></td>");
+		}
+	}
+
+	pix.on('click', 'td', function () {
+	var boja = $("#colorPicker").val();
+	$(this).css("background", boja);
+	});
+}
+
+
+
+
+
+
+$('#send').click(function (e) {
+	e.preventDefault();	
+	makeGrid();
+});
+		
+
+
+
